f61a6beb603af76c0540c703f725a793a957b29d
unknown <hieph@lotusoutsourcing.com>
2023-11-21 15:13:10 +0700

    fetching data

--- src/app/create-page/page.tsx
+++ src/app/create-page/page.tsx
@@ -1,9 +1,22 @@
+'use client'
 import Link from "next/link"
 import x from '../styles/app.module.css'
 import y from '../styles/test.module.css'
 import AppTable from '../components/app.table'
+import { useEffect } from 'react'
 
 export default function CreatePage() {
+
+
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const res = await fetch("http://localhost:8000/blogs")
+			const data = await res.json()
+			console.log(">>> check res:" , data      )
+		}
+		fetchData()
+	}, [])
 	return (
 		<div>
 			<ul>
